8a64f29506cf24a02d6ba2d3a1e49e9ac9bf52c6
Damian <dm1988air@gmail.com>
2019-03-11 19:19:00 +0100

    improvements in script file

--- AzureResourcesDelete.ps1
+++ AzureResourcesDelete.ps1
@@ -1,10 +1,12 @@
-#Requires -Version 3.0
-param([String[]] $ResourcesToRemove, [String] $ResourceGroupName)
+param(
+	 [Parameter(Mandatory=$True)]
+	 [String] $ResourceGroupName,
+	 [Parameter(Mandatory=$True)]
+	 [String[]] $ResourcesToRemove)
 
 Write-Host "This script will remove all resources in resource group."
-$host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 foreach ($resourceName in $ResourcesToRemove)
 {
+	Write-Host "Removig resource with name:$resourceName from a resource group: $ResourceGroupName"
 	Remove-AzureRmResource -ResourceName $resourceName -ResourceType Microsoft.Storage/storageAccounts -ResourceGroupName $ResourceGroupName
 }
-
